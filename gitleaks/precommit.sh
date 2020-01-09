318b441accb3feefb81b399a15d8a011e50c3e54
Bryan Alexander <bryan.alexander@gsa.gov>
2020-01-09 15:20:36 -0600

    updates precommit.sh

--- gitleaks/precommit.sh
+++ gitleaks/precommit.sh
@@ -10,7 +10,8 @@ repo_path=$(git rev-parse --show-toplevel)
 hooks_path="${repo_path}/.git/hooks"
 version_path="${hooks_path}/gitleaks_version"
 base_url="https://github.com/GSA/odp-code-repository-commit-rules/releases/download/${expected_version}"
-rules_url="https://raw.githubusercontent.com/GSA/odp-code-repository-commit-rules/${expected_version}/gitleaks/rules.toml"
+raw_url="https://raw.githubusercontent.com/GSA/odp-code-repository-commit-rules/${expected_version}"
+rules_url="${raw_url}/gitleaks/rules.toml"
 
 # read the locally installed version we will need this later
 # to make sure we are running the version the caller has expected
@@ -33,16 +34,16 @@ bin_path="${hooks_path}/${bin_name}"
 # if the gitleaks binary isn't available or it is the wrong version
 # download the expected version
 if [ ! -f "$bin_path" ] || [ "$actual_version" != "$expected_version" ]; then
-    curl -L -o "$bin_path" "${base_url}/${bin_name}"
-    curl -L -o "${hooks_path}/rules.toml" "${rules_url}"
-    echo "$expected_version" > "${hooks_path}/gitleaks_version"
+    curl --silent -L -o "$bin_path" "${base_url}/${bin_name}"
+    curl --silent -L -o "${hooks_path}/rules.toml" "${rules_url}"
+    printf "$expected_version" > "$version_path"
 fi
 
 # if the SHA512 of the downloaded binary doesn't match the
 # sum from the git repository at this tagged version, we 
 # should abort and cleanup our local binary
 actual_sum=$(sha512sum "${bin_path}" | cut -d' ' -f 1)
-expected_sum=$(curl --silent -L "${base_url}/${bin_name}.sum")
+expected_sum=$(curl --silent -L "${raw_url}/gitleaks/sha512/${bin_name}.sum")
 if [ "$actual_sum" != "$expected_sum" ]; then
     echo "SHA512 does not match, cleaning up downloaded binary."
     rm "$bin_path"
